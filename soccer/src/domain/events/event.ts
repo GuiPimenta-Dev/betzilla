30802245bec9fa35af589242f87e797fda31644f
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2023-02-21 14:46:35 -0300

    build: creating soccer microservice

---
+++ soccer/src/domain/events/event.ts
@@ -0,0 +1,8 @@
+
+export class Event {
+  readonly timestamp: Date;
+
+  constructor(public readonly name: string, public readonly payload: any) {
+    this.timestamp = new Date();
+  }
+}
---
+++ soccer/src/infra/brokers/rabbitmq-adapter.ts
@@ -0,0 +1,41 @@
+import amqplib from "amqplib";
+import { Handler } from "../../application/handlers/handler";
+import { Broker } from "../../application/ports/brokers/broker";
+import { Command } from "../../domain/commands/command";
+import { Event } from "../../domain/events/event";
+
+export class RabbitMQAdapter implements Broker {
+  channel: any;
+
+  async connect(): Promise<void> {
+    const connection = await amqplib.connect("amqp://rabbitmq:5672");
+    this.channel = await connection.createChannel();
+  }
+
+  async subscribe(handler: Handler, callback: Function): Promise<void> {
+    await this.channel.assertExchange(handler.name, "fanout", { durable: true });
+    const queue = await this.channel.assertQueue("", { exclusive: true });
+    await this.channel.bindQueue(queue.queue, handler.name, "");
+    await this.channel.consume(
+      queue.queue,
+      async function (msg: any) {
+        if (msg.content) {
+          const input = JSON.parse(msg.content.toString());
+          console.log(input);
+          await callback(input);
+        }
+      },
+      { noAck: true }
+    );
+  }
+
+  async publish(input: Event | Command): Promise<void> {
+    await this.channel.assertExchange(input.name, "fanout", { durable: true });
+    this.channel.publish(input.name, "", Buffer.from(JSON.stringify(input)));
+  }
+
+  async schedule(input: Command): Promise<void> {
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    await this.publish(input);
+  }
+}
