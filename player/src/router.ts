408760354b2d743dab341863a336438df457a2b1
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2023-07-04 17:33:35 -0300

    adding kong to project

--- player/src/router.ts
+++ player/src/router.ts
@@ -3,7 +3,7 @@ import { ExpressAdapter } from "./infra/http/express-adapter";
 
 const app = ExpressAdapter.create();
 
-app.get("/player/:playerId/balance", ExpressAdapter.route(PlayerController.getBalance));
+app.get("/player/balance/:playerId", ExpressAdapter.route(PlayerController.getBalance));
 app.post("/player/signup", ExpressAdapter.route(PlayerController.signup));
 app.post("/player/login", ExpressAdapter.route(PlayerController.login));
 
